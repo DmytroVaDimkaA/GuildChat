fb9085c2cbccb41e835126c1a929be6102ad9f61
DmytroVaDimkaA <166595694+DmytroVaDimkaA@users.noreply.github.com>
2024-06-03 15:21:29 +0000

    v.0.0.9

--- GuildChat/components/AdminSettingsScreen.js
+++ GuildChat/components/AdminSettingsScreen.js
@@ -1,31 +1,86 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Picker, Dimensions } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const AdminSettingsScreen = () => {
-  const [selectedOption, setSelectedOption] = useState('server'); // Состояние для выбранной опции
+const AdminSettingsScreen = ({ servers, parseError }) => {
+  const [selectedOption, setSelectedOption] = useState('server');
+  const [selectedServer, setSelectedServer] = useState(null);
+  const [guildId, setGuildId] = useState('');
+
+  const screenWidth = Dimensions.get('window').width;
+  const buttonWidth = screenWidth * 0.8;
 
   const handleOptionPress = (option) => {
     setSelectedOption(option);
   };
 
+  const handleApplyPress = async () => {
+    try {
+      if (selectedServer) {
+        await AsyncStorage.setItem('game_id', selectedServer.server_name);
+        await AsyncStorage.setItem('role', 'admin');
+        await AsyncStorage.setItem('guild_id', guildId);
+
+        // Здесь можно добавить переход к другому экрану или выполнение других действий
+        console.log('Настройки сохранены!');
+      } else {
+        console.error('Ошибка: сервер не выбран');
+      }
+    } catch (error) {
+      console.error('Ошибка при сохранении настроек:', error);
+    }
+  };
+
+  if (!servers) {
+    return <Text>Loading servers...</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={[styles.button, selectedOption === 'server' && styles.selectedButton]}
-        onPress={() => handleOptionPress('server')}
-      >
-        <Text style={styles.buttonText}>Сервер</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.disabledButton} disabled>
-        <Text style={styles.disabledButtonText}>Світ</Text>
-      </TouchableOpacity>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Id гільдії</Text>
-        <TextInput style={styles.input} editable={false} /> 
+      <View style={styles.contentContainer}>
+        <TouchableOpacity
+          style={[styles.button, selectedOption === 'server' && styles.selectedButton, { width: buttonWidth }]}
+          onPress={() => handleOptionPress('server')}
+        >
+          <Text style={styles.buttonText}>Сервер</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.disabledButton, { width: buttonWidth }]} disabled>
+          <Text style={styles.disabledButtonText}>Світ</Text>
+        </TouchableOpacity>
+
+        {selectedOption === 'server' && (
+          <Picker
+            style={{ width: buttonWidth }}
+            selectedValue={selectedServer}
+            onValueChange={(itemValue) => setSelectedServer(itemValue)}
+          >
+            <Picker.Item label="Выберите сервер" value={null} />
+            {servers.map((server) => (
+              <Picker.Item label={server.name} value={server} key={server.server_name} />
+            ))}
+          </Picker>
+        )}
+
+        <View style={[styles.inputContainer, { width: buttonWidth }]}>
+          <Text style={styles.inputLabel}>Id гільдії</Text>
+          <TextInput
+            style={styles.input}
+            value={guildId}
+            onChangeText={setGuildId}
+            editable={selectedOption === 'server'}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={[styles.button, { width: buttonWidth }]}
+          onPress={handleApplyPress}
+          disabled={!selectedServer}
+        >
+          <Text style={styles.buttonText}>Застосувати</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.disabledButton} disabled>
-        <Text style={styles.disabledButtonText}>Застосувати</Text>
-      </TouchableOpacity>
+
+      {parseError && <Text style={styles.errorText}>{parseError}</Text>}
     </View>
   );
 };
@@ -34,36 +89,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff', // Белый фон как в Telegram
+    backgroundColor: '#f8f8f8',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  contentContainer: {},
   button: {
-    backgroundColor: '#0088cc', // Синий цвет кнопки
-    padding: 15,
-    borderRadius: 5,
+    backgroundColor: '#29ABE2',
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 20,
     marginBottom: 10,
     width: '100%',
     alignItems: 'center',
   },
   selectedButton: {
-    backgroundColor: '#006699', // Более темный синий для выбранной кнопки
+    backgroundColor: '#2798c8',
   },
   disabledButton: {
-    backgroundColor: '#e0e0e0', // Серый цвет для неактивной кнопки
-    padding: 15,
-    borderRadius: 5,
+    backgroundColor: '#B0B0B0',
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 20,
     marginBottom: 10,
     width: '100%',
     alignItems: 'center',
   },
   buttonText: {
     color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: 16,
+    fontWeight: '500',
   },
   disabledButtonText: {
-    color: '#999999', // Более светлый серый для текста неактивной кнопки
-    fontSize: 18,
-    fontWeight: 'bold',
+    color: '#666666',
+    fontSize: 16,
+    fontWeight: '500',
   },
   inputContainer: {
     marginBottom: 10,
@@ -71,15 +131,21 @@ const styles = StyleSheet.create({
   inputLabel: {
     fontSize: 16,
     marginBottom: 5,
+    color: '#333333',
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc', // Светло-серый цвет границы
+    borderColor: '#e0e0e0',
     padding: 10,
     borderRadius: 5,
+    backgroundColor: '#f2f2f2',
+  },
+  buttonWidth: {
+    width: '80%',
+  },
+  errorText: {
+    color: 'red',
   },
 });
 
 export default AdminSettingsScreen;
-
-
