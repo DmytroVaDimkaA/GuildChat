2548250c605597c73db1cf90eddecac33e496a3d
DmytroVaDimkaA <166595694+DmytroVaDimkaA@users.noreply.github.com>
2024-06-03 13:09:14 +0000

    v.0.0.6

--- GuildChat/parser.js
+++ GuildChat/parser.js
@@ -1,34 +1,42 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+
 export async function parseData() {
   try {
     const response = await axios.get('https://foe.scoredb.io/Worlds');
     const $ = cheerio.load(response.data);
 
+
     const serversByCountry = {};
 
+
     const serverList = $('ul.navbar-nav:has(a:contains("Servers"))');
+
 
     serverList.find('.dropdown').each((i, countryDropdown) => {
       const country = $(countryDropdown).find('.dropdown-item:first-child img').attr('alt');
       const flagUrl = 'https://foe.scoredb.io' + $(countryDropdown).find('.dropdown-item:first-child img').attr('src');
 
+
       serversByCountry[country] = [];
+
 
       $(countryDropdown).find('.dropdown-menu .dropdown-item').each((j, serverItem) => {
         if ($(serverItem).find('img').length === 0) {
           const serverName = $(serverItem).text().trim();
           const serverUrl = $(serverItem).attr('href');
 
+
+          // Обрезаем начало адреса и переименовываем ключ
           serversByCountry[country].push({
             name: serverName,
-            server_name: serverUrl.replace('https://foe.scoredb.io/', ''),
-            flagUrl: flagUrl, // Добавляем URL флага в каждый объект сервера
+            server_name: serverUrl.replace('https://foe.scoredb.io/', '')
           });
         }
       });
     });
+
 
     return serversByCountry;
   } catch (error) {
