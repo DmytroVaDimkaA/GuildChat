cd89930458e152924b8a9f0d54ccc71d477d13fa
DmytroVaDimkaA <166595694+DmytroVaDimkaA@users.noreply.github.com>
2024-06-03 17:46:27 +0000

    v.0.0.9

--- GuildChat/components/AdminSettingsScreen.js
+++ GuildChat/components/AdminSettingsScreen.js
@@ -1,45 +1,21 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, Picker, Dimensions } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 
-const AdminSettingsScreen = ({ servers, parseError }) => {
+const AdminSettingsScreen = () => {
   const [selectedOption, setSelectedOption] = useState('server');
-  const [selectedServer, setSelectedServer] = useState(null);
-  const [guildId, setGuildId] = useState('');
-
-  const screenWidth = Dimensions.get('window').width;
-  const buttonWidth = screenWidth * 0.8;
 
   const handleOptionPress = (option) => {
     setSelectedOption(option);
   };
 
-  const handleApplyPress = async () => {
-    try {
-      if (selectedServer) {
-        await AsyncStorage.setItem('game_id', selectedServer.server_name);
-        await AsyncStorage.setItem('role', 'admin');
-        await AsyncStorage.setItem('guild_id', guildId);
-
-        // Здесь можно добавить переход к другому экрану или выполнение других действий
-        console.log('Настройки сохранены!');
-      } else {
-        console.error('Ошибка: сервер не выбран');
-      }
-    } catch (error) {
-      console.error('Ошибка при сохранении настроек:', error);
-    }
-  };
-
-  if (!servers) {
-    return <Text>Loading servers...</Text>;
-  }
+  const screenWidth = Dimensions.get('window').width;
+  const buttonWidth = screenWidth * 0.8; // 80% от ширины экрана
 
   return (
     <View style={styles.container}>
-      <View style={styles.contentContainer}>
+      <View style={styles.contentContainer}> // Добавлен контейнер для центрирования элементов
         <TouchableOpacity
-          style={[styles.button, selectedOption === 'server' && styles.selectedButton, { width: buttonWidth }]}
+          style={[styles.button, selectedOption === 'server' && styles.selectedButton, { width: buttonWidth }]} // Фиксированная ширина
           onPress={() => handleOptionPress('server')}
         >
           <Text style={styles.buttonText}>Сервер</Text>
@@ -47,40 +23,14 @@ const AdminSettingsScreen = ({ servers, parseError }) => {
         <TouchableOpacity style={[styles.disabledButton, { width: buttonWidth }]} disabled>
           <Text style={styles.disabledButtonText}>Світ</Text>
         </TouchableOpacity>
-
-        {selectedOption === 'server' && (
-          <Picker
-            style={{ width: buttonWidth }}
-            selectedValue={selectedServer}
-            onValueChange={(itemValue) => setSelectedServer(itemValue)}
-          >
-            <Picker.Item label="Выберите сервер" value={null} />
-            {servers.map((server) => (
-              <Picker.Item label={server.name} value={server} key={server.server_name} />
-            ))}
-          </Picker>
-        )}
-
         <View style={[styles.inputContainer, { width: buttonWidth }]}>
           <Text style={styles.inputLabel}>Id гільдії</Text>
-          <TextInput
-            style={styles.input}
-            value={guildId}
-            onChangeText={setGuildId}
-            editable={selectedOption === 'server'}
-          />
+          <TextInput style={styles.input} editable={false} />
         </View>
-
-        <TouchableOpacity
-          style={[styles.button, { width: buttonWidth }]}
-          onPress={handleApplyPress}
-          disabled={!selectedServer}
-        >
-          <Text style={styles.buttonText}>Застосувати</Text>
+        <TouchableOpacity style={[styles.disabledButton, { width: buttonWidth }]} disabled>
+          <Text style={styles.disabledButtonText}>Застосувати</Text>
         </TouchableOpacity>
       </View>
-
-      {parseError && <Text style={styles.errorText}>{parseError}</Text>}
     </View>
   );
 };
@@ -89,39 +39,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f8f8f8',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f8f8f8', // Светло-серый фон Telegram
+    alignItems: 'center', // Выравнивание по горизонтали
+    justifyContent: 'center', // Выравнивание по вертикали
   },
-  contentContainer: {},
+  contentContainer: {}, // Пустой контейнер для центрирования элементов
   button: {
-    backgroundColor: '#29ABE2',
+    backgroundColor: '#29ABE2', // Голубой цвет активной кнопки Telegram
     paddingHorizontal: 20,
     paddingVertical: 12,
-    borderRadius: 20,
+    borderRadius: 20, // Увеличенный радиус для более скругленных углов
     marginBottom: 10,
-    width: '100%',
     alignItems: 'center',
   },
   selectedButton: {
-    backgroundColor: '#2798c8',
+    backgroundColor: '#2798c8', // Более темный голубой для выбранной кнопки
   },
   disabledButton: {
-    backgroundColor: '#B0B0B0',
+    backgroundColor: '#B0B0B0', // Серый цвет для неактивной кнопки Telegram
     paddingHorizontal: 20,
     paddingVertical: 12,
     borderRadius: 20,
     marginBottom: 10,
-    width: '100%',
     alignItems: 'center',
   },
   buttonText: {
     color: 'white',
-    fontSize: 16,
-    fontWeight: '500',
+    fontSize: 16, // Уменьшенный размер шрифта
+    fontWeight: '500', // Средний вес шрифта
   },
   disabledButtonText: {
-    color: '#666666',
+    color: '#666666', // Более темный серый для текста неактивной кнопки
     fontSize: 16,
     fontWeight: '500',
   },
@@ -131,20 +79,14 @@ const styles = StyleSheet.create({
   inputLabel: {
     fontSize: 16,
     marginBottom: 5,
-    color: '#333333',
+    color: '#333333', // Темно-серый цвет текста
   },
   input: {
     borderWidth: 1,
-    borderColor: '#e0e0e0',
+    borderColor: '#e0e0e0', // Более светлый серый для границы
     padding: 10,
     borderRadius: 5,
-    backgroundColor: '#f2f2f2',
-  },
-  buttonWidth: {
-    width: '80%',
-  },
-  errorText: {
-    color: 'red',
+    backgroundColor: '#f2f2f2', // Светло-серый фон поля ввода
   },
 });
 
