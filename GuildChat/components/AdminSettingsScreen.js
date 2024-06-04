908ec4b9fcffddc7d32555c03a9e025264215d59
DmytroVaDimkaA <166595694+DmytroVaDimkaA@users.noreply.github.com>
2024-06-04 20:54:36 +0000

    v.0.1.1

--- GuildChat/components/AdminSettingsScreen.js
+++ GuildChat/components/AdminSettingsScreen.js
@@ -17,6 +17,91 @@ const AdminSettingsScreen = () => {
 
   const placeholderTextColor = '#999999';
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: '#f8f8f8',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    contentContainer: {},
+    button: {
+      backgroundColor: '#29ABE2',
+      paddingHorizontal: 20,
+      paddingVertical: 12,
+      borderRadius: 5,
+      marginBottom: 10,
+      alignItems: 'center',
+    },
+    selectedButton: {
+      backgroundColor: '#0088CC',
+    },
+    disabledButton: {
+      backgroundColor: '#B0B0B0',
+      paddingHorizontal: 20,
+      paddingVertical: 12,
+      borderRadius: 5,
+      marginBottom: 10,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: '500',
+    },
+    disabledButtonText: {
+      color: '#666666',
+      fontSize: 16,
+      fontWeight: '500',
+    },
+    inputContainer: {
+      marginBottom: 10,
+    },
+    inputLabel: {
+      fontSize: 16,
+      marginBottom: 5,
+      color: '#333333',
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#e0e0e0',
+      padding: 10,
+      borderRadius: 5,
+      backgroundColor: '#f2f2f2',
+    },
+    placeholderText: {
+      color: '#999999',
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+      backgroundColor: '#fff',
+      padding: 20,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+    },
+    modalTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    modalButton: {
+      backgroundColor: '#29ABE2',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+    },
+    modalButtonText: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
@@ -63,89 +148,5 @@ const AdminSettingsScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#f8f8f8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  contentContainer: {},
-  button: {
-    backgroundColor: '#29ABE2',
-    paddingHorizontal: 20,
-    paddingVertical: 12,
-    borderRadius: 5,
-    marginBottom: 10,
-    alignItems: 'center',
-  },
-  selectedButton: {
-    backgroundColor: '#0088CC',
-  },
-  disabledButton: {
-    backgroundColor: '#B0B0B0',
-    paddingHorizontal: 20,
-    paddingVertical: 12,
-    borderRadius: 5,
-    marginBottom: 10,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  disabledButtonText: {
-    color: '#666666',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  inputContainer: {
-    marginBottom: 10,
-  },
-  inputLabel: {
-    fontSize: 16,
-    marginBottom: 5,
-    color: '#333333',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#e0e0e0',
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: '#f2f2f2',
-  },
-  placeholderText: {
-    color: '#999999',
-  },
-  modalContainer: { // Стили для модального окна
-    flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    padding: 20,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-  },
-  modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  modalButton: {
-    backgroundColor: '#29ABE2',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  modalButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
-
 export default AdminSettingsScreen;
+
--- GuildChat/parser.js
+++ GuildChat/parser.js
@@ -6,33 +6,35 @@ export async function parseData() {
     const response = await axios.get('https://foe.scoredb.io/Worlds');
     const $ = cheerio.load(response.data);
 
-    // Используем объект для группировки серверов по странам
-    const serversByCountry = {}; 
+    const serversByCountry = {};
 
     const serverList = $('ul.navbar-nav:has(a:contains("Servers"))');
 
     serverList.find('.dropdown').each((i, countryDropdown) => {
       const country = $(countryDropdown).find('.dropdown-item:first-child img').attr('alt');
+      const flagUrl = 'https://foe.scoredb.io' + $(countryDropdown).find('.dropdown-item:first-child img').attr('src');
 
-      serversByCountry[country] = []; 
+      serversByCountry[country] = [];
 
       $(countryDropdown).find('.dropdown-menu .dropdown-item').each((j, serverItem) => {
         if ($(serverItem).find('img').length === 0) {
           const serverName = $(serverItem).text().trim();
           const serverUrl = $(serverItem).attr('href');
 
-          serversByCountry[country].push({ 
-            name: serverName, 
-            server_name: serverUrl.replace('https://foe.scoredb.io/', '') 
+          serversByCountry[country].push({
+            name: serverName,
+            server_name: serverUrl.replace('https://foe.scoredb.io/', ''),
+            flagUrl: flagUrl, // Добавляем URL флага
           });
         }
       });
     });
 
-    return serversByCountry; // Возвращаем объект
+    return serversByCountry;
   } catch (error) {
     console.error('Ошибка при парсинге:', error);
-    throw error;
+    throw error; // Передаем ошибку дальше для обработки
   }
 }
+
 
