59fbca3e1e6f1fc3f035e9378a0637aec6a3b59c
DmytroVaDimkaA75 <vadimkaa75@gmail.com>
2024-06-27 17:52:26 +0000

    v.0.1.17

--- GuildChat/components/AdminSelectScreen.js
+++ GuildChat/components/AdminSelectScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, FlatList, StyleSheet, Image, Modal, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { database } from '../firebaseConfig'; // Припустимо, що це ваш імпорт бази даних Firebase
-import { ref, set } from 'firebase/database';
+import { ref, set, get, update } from 'firebase/database';
+import CryptoJS from 'react-native-crypto-js';
 
-const AdminSelectScreen = ({ guildData, clanCaption, uril, guildId }) => {
+const AdminSelectScreen = ({ guildData, clanCaption, guildId, uril }) => {
   const [selectedMember, setSelectedMember] = useState(null);
   const [imageLoadingStates, setImageLoadingStates] = useState({});
 
@@ -13,56 +14,79 @@ const AdminSelectScreen = ({ guildData, clanCaption, uril, guildId }) => {
 
   const handleConfirm = () => {
     if (selectedMember) {
-      const userId = selectedMember.linkUrl.split('/').pop(); // Отримати останній шматок URL як userId
-      const imageUrl = `https://foe.scoredb.io${selectedMember.imageUrl}`; // imageUrl користувача
-      const updatedGuildId = `${uril}_${guildId}`; // Формування нового значення guildId
-      const userName = selectedMember.name; // Отримання userName
-      const role = "member"; // Визначення ролі користувача (у даному випадку це просто "member", але може бути динамічним)
-  
-      // Функція для оновлення даних користувача у базі даних Firebase
-      const updateUserDetails = (userId, guildId, imageUrl, userName, role) => {
-        const userRef = ref(database, `users/${userId}`);
-        const userGuildRef = ref(database, `users/${userId}/${guildId}`);
-        return Promise.all([
-          set(userGuildRef, { imageUrl, role }),
-          set(userRef, { userName, [`${guildId}`]: { imageUrl, role } })
-        ]);
+      const selectedUserId = selectedMember.linkUrl.split('/').pop(); // Отримати останній шматок URL як userId
+
+      // Створення запису в гілці guilds
+      const guildRef = ref(database, `guilds/${uril}_${guildId}`);
+      const guildInfo = {
+        guildName: clanCaption,
       };
-  
-      // Функція для створення нової гілки у базі даних Firebase
-      const createGuildBranch = (guildId, guildName) => {
-        const guildRef = ref(database, `guilds/${guildId}`);
-        return set(guildRef, { guildName });
-      };
-  
-      updateUserDetails(userId, updatedGuildId, imageUrl, userName, role)
+
+      set(guildRef, guildInfo)
         .then(() => {
-          console.log(`Дані користувача оновлено у Firebase для користувача з userId: ${userId}, guildId: ${updatedGuildId}`);
-  
-          // Створення нової гілки у базі даних Firebase
-          return createGuildBranch(updatedGuildId, clanCaption);
-        })
-        .then(() => {
-          console.log(`Гілку guilds/${updatedGuildId} створено з guildName: ${clanCaption}`);
-  
+          console.log(`Дані гільдії оновлено в Firebase для гільдії з id: ${uril}_${guildId}`);
+
+          // Додавання членів гільдії до гілки users
+          guildData.forEach((member) => {
+            const userId = member.linkUrl.split('/').pop();
+            const imageUrl = `https://foe.scoredb.io${member.imageUrl}`;
+            const userGuildData = {
+              [`${uril}_${guildId}`]: {
+                imageUrl: imageUrl,
+                role: userId === selectedUserId ? 'guildLeader' : 'member', // Визначення ролі
+              }
+            };
+
+            // Перевірка існування користувача
+            get(ref(database, `users/${userId}`))
+              .then((snapshot) => {
+                if (snapshot.exists()) {
+                  // Користувач існує, оновлюємо дані про нову гільдію
+                  update(ref(database, `users/${userId}`), userGuildData)
+                    .then(() => {
+                      console.log(`Дані користувача оновлено в Firebase для гільдії з id: ${uril}_${guildId}`);
+                    })
+                    .catch((error) => {
+                      console.error('Помилка при оновленні даних користувача:', error);
+                    });
+                } else {
+                  // Користувач не існує, створюємо новий запис
+                  const userRootRef = ref(database, `users/${userId}`);
+                  const encryptedUserId = CryptoJS.AES.encrypt(userId, 'your-encryption-key').toString();
+                  const userRootData = {
+                    userName: member.name,
+                    password: encryptedUserId, // Додавання зашифрованого userId
+                    ...userGuildData // Додаємо дані про гільдію
+                  };
+
+                  set(userRootRef, userRootData)
+                    .then(() => {
+                      console.log(`Основні дані користувача оновлено в Firebase для користувача з userId: ${userId}`);
+                    })
+                    .catch((error) => {
+                      console.error('Помилка при оновленні основних даних користувача:', error);
+                    });
+                }
+              })
+              .catch((error) => {
+                console.error('Помилка при перевірці існування користувача:', error);
+              });
+          });
+
           // Виведення отриманих даних з AdminSettingsScreen у консоль
           console.log("Отримані дані з AdminSettingsScreen в AdminSelectScreen:");
           console.log("guildData:", guildData);
           console.log("clanCaption:", clanCaption);
-          console.log("uril:", uril);
           console.log("guildId:", guildId);
-          console.log("updatedGuildId:", updatedGuildId); // Вивід нової константи у консоль
-  
+
           setSelectedMember(null);
         })
         .catch((error) => {
-          console.error('Помилка при оновленні даних користувача або створенні гілки:', error);
+          console.error('Помилка при оновленні даних гільдії:', error);
         });
     }
   };
-  
-  
-  
+
   const handleCancel = () => {
     setSelectedMember(null);
   };
@@ -111,7 +135,7 @@ const AdminSelectScreen = ({ guildData, clanCaption, uril, guildId }) => {
               <>
                 <Image source={{ uri: `https://foe.scoredb.io${selectedMember.imageUrl}` }} style={styles.modalImage} />
                 <Text style={styles.modalName}>{selectedMember.name}</Text>
-                <Text style={styles.confirmationText}>Ви підтверджує свій акаунт?</Text>
+                <Text style={styles.confirmationText}>Ви підтверджуєте свій акаунт?</Text>
                 <View style={styles.buttonContainer}>
                   <TouchableOpacity onPress={handleConfirm} style={styles.button}>
                     <Text style={styles.buttonText}>Підтвердити</Text>
@@ -179,52 +203,46 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    width: 300,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     padding: 20,
     borderRadius: 10,
     alignItems: 'center',
   },
   modalImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    marginBottom: 15,
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+    borderRadius: 50,
   },
   modalName: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 20,
   },
   confirmationText: {
     fontSize: 16,
-    marginBottom: 20,
-    textAlign: 'center',
+    marginBottom: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    justifyContent: 'space-around',
+    marginTop: 20,
   },
   button: {
-    flex: 1,
-    backgroundColor: '#64B5F6',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    backgroundColor: '#2196F3',
+    padding: 10,
     borderRadius: 5,
-    alignItems: 'center',
     marginHorizontal: 5,
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
   },
   errorText: {
-    textAlign: 'center',
     color: 'red',
+    textAlign: 'center',
     marginTop: 20,
   },
 });
 
 export default AdminSelectScreen;
-
