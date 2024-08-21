374c801ca4c4b564e3f9bacea5afead28560b72c
DmytroVaDimkaA <166595694+DmytroVaDimkaA@users.noreply.github.com>
2024-08-21 21:35:04 +0000

    v.0.2.7

--- GuildChat/components/GB/MyGB.js
+++ GuildChat/components/GB/MyGB.js
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, ScrollView, Image } from 'react-native';
-import { ref, get, onValue, off } from 'firebase/database';
-import { database } from '../../firebaseConfig'; // Шлях до конфігурації Firebase
+import { View, Text, StyleSheet, ActivityIndicator, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { ref, get } from 'firebase/database';
+import { database } from '../../firebaseConfig';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons';
+
+// Компонент для відображення бонусу
+const BonusView = ({ bonus }) => (
+  <View style={styles.bonusContainer}>
+    <Text style={styles.buildBonus}>Bonus: {bonus}</Text>
+  </View>
+);
+
+// Компонент для відображення решти інформації
+const DetailsView = ({ build }) => (
+  <View style={styles.detailsContainer}>
+    {Object.keys(build).map((key, index) =>
+      key !== 'id' && key !== 'bonus' && key !== 'buildingImage' && key !== 'buildingName' && (
+        <View key={index} style={styles.detailContainer}>
+          <Text style={styles.detailKey}>{key}:</Text>
+          <Text style={styles.detailValue}>{JSON.stringify(build[key])}</Text>
+        </View>
+      )
+    )}
+  </View>
+);
 
 const MyGB = () => {
   const [greatBuilds, setGreatBuilds] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [expandedBuildId, setExpandedBuildId] = useState(null);
 
   useEffect(() => {
     const fetchGreatBuilds = async () => {
       try {
-        // Отримання guildId і userId з AsyncStorage
         const storedGuildId = await AsyncStorage.getItem('guildId');
         const storedUserId = await AsyncStorage.getItem('userId');
 
@@ -20,7 +42,6 @@ const MyGB = () => {
           throw new Error('Guild ID or User ID not found in AsyncStorage');
         }
 
-        // Отримання даних про великі будівлі користувача
         const guildsRef = ref(database, `guilds/${storedGuildId}/guildUsers/${storedUserId}/greatBuild`);
         const greatBuildingsRef = ref(database, 'greatBuildings');
 
@@ -28,12 +49,10 @@ const MyGB = () => {
         if (buildSnapshot.exists()) {
           const buildData = buildSnapshot.val();
 
-          // Отримання даних про всі великі будівлі
           const buildingsSnapshot = await get(greatBuildingsRef);
           if (buildingsSnapshot.exists()) {
             const buildingsData = buildingsSnapshot.val();
 
-            // Форматування та об'єднання даних
             const buildsList = Object.keys(buildData).map(key => ({
               id: key,
               ...buildData[key]
@@ -64,13 +83,14 @@ const MyGB = () => {
       }
     };
 
-    fetchGreatBuilds(); // Виклик функції отримання даних
+    fetchGreatBuilds();
 
-    // Очищення підписок, якщо вони є
-    return () => {
-      // Тепер ви можете не використовувати `off` у цьому випадку, якщо не використовуються підписки
-    };
+    return () => {};
   }, []);
+
+  const handleToggle = (id) => {
+    setExpandedBuildId(expandedBuildId === id ? null : id);
+  };
 
   if (loading) {
     return <ActivityIndicator size="large" color="#0000ff" />;
@@ -87,25 +107,44 @@ const MyGB = () => {
       ) : (
         greatBuilds.map(build => (
           <View key={build.id} style={styles.buildItem}>
-            <View style={styles.imageContainer}>
-              {build.buildingImage ? (
-                <Image source={{ uri: build.buildingImage }} style={styles.buildingImage} />
-              ) : (
-                <Text>Image not available</Text>
-              )}
+            <TouchableOpacity style={styles.closeButton}>
+              <Ionicons name="close" size={24} color="black" />
+            </TouchableOpacity>
+            <View style={styles.imageNameContainer}>
+              <View style={styles.imageContainer}>
+                {build.buildingImage ? (
+                  <Image source={{ uri: build.buildingImage }} style={styles.buildingImage} />
+                ) : (
+                  <Text>Image not available</Text>
+                )}
+              </View>
               <View style={styles.nameContainer}>
-                <Text style={styles.buildName}>{build.buildingName}</Text>
+                <View style={styles.nameBlock}>
+                  <Text style={styles.buildName}>{build.buildingName}</Text>
+                </View>
+                <View style={styles.additionalLevelBlock}>
+                  <View style={styles.additionalLevelText}>
+                    <Text>Рівень:</Text>
+                  </View>
+                  <View style={styles.additionalLevelStepper}>
+                    <Text>Block 1</Text>
+                  </View>
+                </View>
               </View>
             </View>
-            <View style={styles.detailsContainer}>
-              <Text style={styles.buildBonus}>Bonus: {build.bonus}</Text>
-              {Object.keys(build).map((key, index) => key !== 'id' && key !== 'bonus' && key !== 'buildingImage' && key !== 'buildingName' && (
-                <View key={index} style={styles.detailContainer}>
-                  <Text style={styles.detailKey}>{key}:</Text>
-                  <Text style={styles.detailValue}>{JSON.stringify(build[key])}</Text>
-                </View>
-              ))}
-            </View>
+            <TouchableOpacity onPress={() => handleToggle(build.id)} style={styles.chevronContainer}>
+              <Ionicons
+                name={expandedBuildId === build.id ? "chevron-up" : "chevron-down"}
+                size={24}
+                color="black"
+              />
+            </TouchableOpacity>
+            {expandedBuildId === build.id && (
+              <>
+                <BonusView bonus={build.bonus} />
+                <DetailsView build={build} />
+              </>
+            )}
           </View>
         ))
       )}
@@ -126,33 +165,88 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 15,
     padding: 10,
+    position: 'relative',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    zIndex: 1,
+  },
+  imageNameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   imageContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    borderWidth: 1,
-    borderColor: '#000',
-    padding: 5,
-    marginBottom: 10,
+    width: 100,
+    height: 100,
+    borderWidth: 2,
+    borderColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
     backgroundColor: '#e0e0e0',
   },
   buildingImage: {
-    width: 100,
-    height: 100,
+    width: '100%',
+    height: '100%',
     resizeMode: 'cover',
   },
   nameContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 10,
+    borderWidth: 2,
+    borderColor: 'blue',
+    justifyContent: 'flex-start', // Вирівнювання блоків по верхньому краю
+    alignItems: 'stretch', // Розтягує блоки на всю ширину
+    backgroundColor: '#e0e0e0',
   },
-  emptyContainer: {
+  nameBlock: {
+    borderWidth: 1,
+    borderColor: 'green', // Колір рамки для nameBlock
+    padding: 5,
+    alignItems: 'center',
+    // Висота буде автоматично підганятися під висоту тексту
+  },
+  additionalLevelBlock: {
+    flex: 1,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: 'orange', // Колір рамки для additionalLevelBlock
+    
+    alignItems: 'center'
+    //padding: 5,
+  },
+  additionalLevelText: {
+    flex: 1,
+    //margin: 5,
+    //padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    alignItems: 'center',
+  },
+  additionalLevelStepper: {
+    flex: 1,
+    //margin: 5,
+    //padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    alignItems: 'center',
+  },
+
+
+  chevronContainer: {
+    position: 'absolute',
+    bottom: 5,
+    right: 5,
+    zIndex: 1,
+  },
+  bonusContainer: {
     borderWidth: 1,
     borderColor: '#000',
-    height: 20,
-    marginBottom: 10,
-    backgroundColor: '#e0e0e0',
+    padding: 5,
+    backgroundColor: '#ffffff',
   },
   detailsContainer: {
     borderWidth: 1,
@@ -161,7 +255,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   buildName: {
-    fontSize: 22,
+    fontSize: 18,
     fontWeight: 'bold',
   },
   buildBonus: {
